eb527dd7dacaa4ad96ef024ae13f9e10026199fa
MohammadBorabadi0 <MohammadBorabadi0@gmail.com>
2022-06-12 09:11:56 -0700

    fix bug-2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
     const currentDate = new Date().getFullYear();
 
     return (
-        <footer className='flex mt-4 py-6 bg-slate-900'>
+        <footer className='flex mb-20 sm:mb-0 mt-4 py-6 bg-slate-900'>
             <section className='px-3 xl:px-0 md:max-w-7xl md:mx-auto'>
-                <div className='flex justify-center items-center text-lg text-white'>
+                <div className='flex justify-center items-center text-sm sm:text-lg text-white'>
                     <h3>© {currentDate} 7Shop
                         All rights reserved</h3>
                 </div>
